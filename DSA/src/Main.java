3be8527e53a64f575990fb52c24dea242d6106f6
HarshitBisht <harshitbisht282@gmail.com>
2024-06-01 14:02:47 +0530

    added sop log

--- DSA/src/Main.java
+++ DSA/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String arg[]) {
         int[] arr = {6, 2, 3, 4, 7, 2, 1, 7, 1};
         System.out.println("bbb" + MaximumPointsYouCanObtainFromCards.maximumPointsYouCanObtainFromCards(arr, 4));
-        //gkkkk
+        System.out.print("lion");
     }
 }
 
