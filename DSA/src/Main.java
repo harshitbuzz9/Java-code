444f74b645db8c26c263963bd1ccdbbdbc46aa60
Harshit Bisht <harshit.bisht@dgliger.com>
2024-06-01 12:37:29 +0530

    ok

--- DSA/src/Main.java
+++ DSA/src/Main.java
@@ -3,7 +3,8 @@ import twoPointersSubStringPrograms.MaximumPointsYouCanObtainFromCards;
 public class Main {
     public static void main(String arg[]) {
         int[] arr = {6, 2, 3, 4, 7, 2, 1, 7, 1};
-        System.out.println("bbb"+MaximumPointsYouCanObtainFromCards.maximumPointsYouCanObtainFromCards(arr, 4));
+        System.out.println("bbb" + MaximumPointsYouCanObtainFromCards.maximumPointsYouCanObtainFromCards(arr, 4));
+        //gkkkk
     }
 }
 
